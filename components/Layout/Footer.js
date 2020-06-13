92c4909ed90de9179d8fb4de7071f975161aa104
desaijay315 <desaijay315@gmail.com>
2020-06-13 23:12:59 +0530

    footer

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -38,7 +38,7 @@ const Footer = (props) => {
                 </div>
                 </div>
                 <div className="copyright_wrap">
-                <p className="copyright">Copyright@TLG India Pvt. Ltd. Division Indigo Consulting</p>
+                <p className="copyright">OKAY----Copyright@TLG India Pvt. Ltd. Division Indigo Consulting</p>
                 </div>
             </div>
         </footer>
